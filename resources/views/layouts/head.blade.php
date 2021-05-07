89ac9794707ac0ae79a88d231abc1e1b2cc27170
Fahmi Junaedi <fahmi.juna@gmail.com>
2021-05-07 21:04:28 +0700

    icon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ asset('layout/dist/img/logo-kopma-ikon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('layout/dist/img/logo-kopma-icon.png') }}" />
     <title>HRIS | {{$title ?? ''}}</title>
 
     <!-- Google Font: Source Sans Pro -->
--- resources/views/page/auth/login.blade.php
+++ resources/views/page/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{ asset('layout/dist/img/logo-kopma-ikon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('layout/dist/img/logo-kopma-icon.png') }}" />
     <title>HRIS | Login</title>
 
     <!-- Google Font: Source Sans Pro -->
